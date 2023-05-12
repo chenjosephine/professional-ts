2f37c75897d3ce8008a5ff7585dd793dfc3e30e1
chenjosephine <jchen1114@gmail.com>
2023-05-11 22:47:54 -0400

    git commit -m "passes linting"

--- src/ui/components/Channel.tsx
+++ src/ui/components/Channel.tsx
@@ -32,7 +32,7 @@ const Channel: React.FunctionComponent<any> = ({ channel }) => {
         className="py-4 flex-1 overflow-y-scroll channel-messages-list"
         role="list"
       >
-        {messages.map((m) => (
+        {messages.map((m: any) => (
           <ChannelMessage
             key={m.id}
             body={m.body}
